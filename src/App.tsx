bb70e95e2f36b8e791fd07bfd548fac0f6710e49
song <peko@ip-172-31-43-162.ap-northeast-2.compute.internal>
2021-07-14 10:47:27 +0900

    img

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import MainScreen from "@screens/MainScreen";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_HOST;
 
 export const App = () => {
   return (
